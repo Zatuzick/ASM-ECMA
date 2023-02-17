e3b2df431777d3f0b11c56a77f11614d14084d35
minhnv20397 <zatuzick03@gmail.com>
2023-02-17 15:15:43 +0700

    update img

--- src/components/HeaderPage.js
+++ src/components/HeaderPage.js
@@ -10,14 +10,24 @@ const HeaderPage = () => {
       .then((data) => setData(data));
   }, []);
 
+  const [img, setImg] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3000/imgs")
+      .then((response) => response.json())
+      .then((img) => setImg(img));
+  }, []);
+
   return `
   <div class="grid grid-cols-1 md:grid-cols-2   gap-4 border-b-2 border-black ">
   <div>
-      <a href="/"><img src="src/img/ad.jpg" alt="" class="rounded-full mx-auto md:m-2 w-20 h-20 my-2"></a>
+  ${img.map(
+    (img, index) => `
+      <a href="/"><img src="${img.img[0]}" alt="" class="rounded-full mx-auto md:m-2 w-20 h-20 my-2"></a>
+      `).join("")}
   </div>
   <div class="flex my-auto mx-auto">
   ${data.map(
-    (menu, index) => `
+      (menu, index) => `
       <a href="${menu.link}" class="mx-6 font-bold text-2xl hover:text-red-500">${menu.name}</a>
       `).join("")}
   </div>
